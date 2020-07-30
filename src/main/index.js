5f1f05b24ee85e90e4c376a1abbdfbc280e41628
boxlab <boxlab@foxmail.com>
2020-07-31 03:40:20 +0800

    [新增] 开机自启动(#6)待设置页面完成后完善

--- src/main/index.js
+++ src/main/index.js
@@ -102,21 +102,28 @@ function createWindow() {
     })
 }
 
+app.setLoginItemSettings({
+    openAtLogin: false,
+    openAsHidden: false,
+    path: process.execPath,
+    args: [
+        '--processStart', `"${process.execPath}"`,
+    ]
+});
+
 app.on('ready', () => {
     createWindow();
-})
-
+});
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
-
+});
 app.on('activate', () => {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
 ipcMain.on('window-minimize', () => mainWindow.minimize());
 ipcMain.on('window-maximize', () => {
