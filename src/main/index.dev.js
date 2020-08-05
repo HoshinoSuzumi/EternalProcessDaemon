f8447fcb07d75b0500f37433641e9574d53d1800
boxlab <boxlab@foxmail.com>
2020-08-05 20:55:14 +0800

    很抱歉摸了一天鱼 :fish:

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -21,23 +21,52 @@ require('electron').app.on('ready', () => {
         })
 })
 
+let forever = require('forever')
+
+let inst = forever.start(["GifCam"], {
+    silent: true,
+    spinSleepTime: 2000,
+    uid: 'cb48d398-8dd3-4d3d-ba5a-f3a02ba4daf8',
+    max: undefined,
+    cwd: 'G:\\软件\\GifCam',
+    minUptime: 1000,
+})
+
+forever.startServer([inst], (v) => {
+    console.log('server: ' + v)
+})
+
+setInterval(function () {
+    forever.list(true, (data) => {
+        console.log(data)
+    })
+}, 2000)
+
 // let forever = require('forever-monitor');
-// let child = forever.start(["TakeColor.exe"], {
+// let child = forever.start(["GifCam"], {
+//     silent: true,
+//     spinSleepTime: 2000,
+//     uid: 'cb48d398-8dd3-4d3d-ba5a-f3a02ba4daf8',
+//     max: undefined,
+//     cwd: 'G:\\软件\\GifCam',
+//     minUptime: 1000,
+// });
+
+// let child = forever.start(["mstsc.exe"], {
 //     uid: 'cb48d398-8dd3-4d3d-ba5a-f3a02ba4daf8',
 //     max: undefined,
 //     silent: false,
-//     cwd: 'G:\\软件',
+//     cwd: 'C:\\Windows\\System32',
 //     minUptime: 1000,
 //     spinSleepTime: 2000,
-//     pidFile: './pid'
 // });
-//
+
 // child.on('restart', function () {
-//     console.error('Forever restarting script for ' + child.times + ' time');
+//     console.error('Restarting ' + child.uid + ' for ' + child.times + ' time');
 // });
 //
 // child.on('exit:code', function (code) {
-//     console.error('Forever detected ' + child.uid + ' exited with code ' + code);
+//     console.error('Detected ' + child.uid + ' exited with code ' + code);
 // });
 
 // Require `main` process to boot app
